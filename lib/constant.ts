d71fec3b06b3ffb957baef5930a05f78a19f6449
AslamSDM <aslamprpd@gmail.com>
2024-09-11 18:57:22 +0530

    upddate address

--- lib/constant.ts
+++ lib/constant.ts
@@ -1,4 +1,8 @@
+// export const LumanagiPredictionsV3BTC =
+//   "0x8a96D7978Cb1C5EAFd658a78A5Ab6ffD3A572385";
+// export const LumanagiPredictionsV3ETH =
+//   "0x57ebDFc4d6580F6Fbb88CBAF43b8AD12B08d98Fd";
 export const LumanagiPredictionsV3BTC =
-  "0x8a96D7978Cb1C5EAFd658a78A5Ab6ffD3A572385";
+  "0xad9eBfE7E2FD524FD10b9A8b832098E85a01Bf45";
 export const LumanagiPredictionsV3ETH =
-  "0x57ebDFc4d6580F6Fbb88CBAF43b8AD12B08d98Fd";
+  "0x8aF32C3aB156ecdeb51eDBeC0Dce51EdD515375A";
